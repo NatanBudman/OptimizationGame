8f976b43c3752fc2e63ada63d328c02b1f920b8e
NatanBudman <natanbd02@gmail.com>
2023-04-28 16:46:17 -0300

    Add Enemie Spawn

---
+++ Assets/Scriot/Manager/SpawnManager.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+public class SpawnManager : MonoBehaviour, IUpdates
+{
+    [Header("SpawnPoints") ]
+    public Nodo[] EnemySpawns;
+    public Nodo playerSpawn;
+    
+    [Space(5)]
+
+    [Header("Prefabs")] 
+    [Space(2)]
+    public GameObject Enemies;
+    public GameObject Player;
+
+    [Header("Paramatros")]
+    [Space(2)]
+    public int EnemyCount;
+    private int _CurrentEnemies;
+    [Space(2)]
+    public float CoolwdownEnemieInstantiate;
+    private float _CurrentEnemieInstantiate;
+
+//[]
+    void Start()
+    {
+        _CurrentEnemies = EnemyCount;
+    }
+
+    public void UIUpdate()
+    {
+        
+    }
+
+    public void GameplayUpdate()
+    {
+        
+    }
+
+    private void Update()
+    {
+        _CurrentEnemieInstantiate += Time.deltaTime;
+        if (_CurrentEnemieInstantiate > CoolwdownEnemieInstantiate)
+        {
+            var random = Random.Range(0, EnemySpawns.Length);
+            Vector3 pos = new Vector3(EnemySpawns[random].transform.position.x,
+                EnemySpawns[random].transform.position.y + 1f, EnemySpawns[random].transform.position.z);
+            
+            GameObject InstantiateOBJ = Instantiate(Enemies, pos, Quaternion.identity);
+            InstantiateOBJ.GetComponent<GrillaMovement>().StartNodo = EnemySpawns[random];
+            _CurrentEnemieInstantiate = 0;
+        }
+    }
+}
